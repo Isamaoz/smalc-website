127b0c23e033c762f2bda62b8b34935fd9edd31d
Isamiri Ortiz <isamiriobl@gmail.com>
2020-11-30 14:26:17 -0600

    Version 1.0

--- node-mysql-connection/src/app/routes/students.js
+++ node-mysql-connection/src/app/routes/students.js
@@ -22,8 +22,7 @@ module.exports = app => {
             if (results.length > 0) {
                 request.session.loggedin = true;
                 request.session.username = username;
-                isHidden = false;
-                response.redirect('/');
+                response.redirect('../logged-in');
             } else {
                 response.send('Incorrect Username and/or Password!');
             }
@@ -54,6 +53,15 @@ app.get('/show-students', (req, res) => {
   });
 });
 
+app.get('/logged-in', (req, res) => {
+  connection.query('SELECT * FROM students', (err, result) => {
+    console.log(result);
+    res.render('logged-in ', {
+      studentlist: result
+    });
+  });
+});
+
 const getByUsername = (pUser) => {
   return new Promise((resolve,reject) => {
     connection.query('SELECT * FROM credentials WHERE username = ?', [pUser],(err, rows) => {
@@ -62,53 +70,5 @@ const getByUsername = (pUser) => {
       });
   });
 };
-
-// app.post('/', function(request, response) {
-// 	var username = request.body.username;
-// 	var password = request.body.password;
-// 	if (username && password) {
-// 		connection.query('SELECT * FROM credentials WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-// 			if (results.length > 0) {
-// 				request.session.loggedin = true;
-// 				request.session.username = username;
-//         isHidden = false;
-// 			} else {
-// 				response.send('Incorrect Username and/or Password!');
-// 			}
-// 			response.end();
-// 		});
-// 	} else {
-// 		response.send('Please enter Username and Password!');
-// 		response.end();
-// 	}
-// });
-//
-// app.post('/', async (req, res) => {
-//     try{
-//         let foundUser = links.find((data) => req.body.email === data.email);
-//         if (foundUser) {
-//
-//             let submittedPass = req.body.password;
-//             let storedPass = foundUser.password;
-//
-//             const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
-//             if (passwordMatch) {
-//                 let usrname = foundUser.username;
-//                 res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
-//             } else {
-//                 res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
-//             }
-//         }
-//         else {
-//
-//             let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
-//             await bcrypt.compare(req.body.password, fakePass);
-//
-//             res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align='center'><a href='./login.html'>login again<a><div>");
-//         }
-//     } catch{
-//         res.send("Internal server error");
-//     }
-// });
 
 }
